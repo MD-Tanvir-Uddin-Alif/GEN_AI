4106c109282004714419269770942194675ddde6
Md. Tanvir Uddin Alif <alif252669@gmail.com>
2026-01-05 10:26:12 +0600

    route created

--- Learning/ChatBot/backend/main.py
+++ Learning/ChatBot/backend/main.py
@@ -1,37 +1,59 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-
-import streamlit as st
+from fastapi import FastAPI
+from pydantic import BaseModel
 from dotenv import load_dotenv
+
 import os
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
 
 load_dotenv()
 
 
+app = FastAPI(title="LangChain Gemini API")
+
+
+#-----------------------------------
+# Request Schema
+#-----------------------------------
+class QuesryRequest(BaseModel):
+    query: str
+
+
+#-----------------------------------
+# Langchain Setup
+#-----------------------------------
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a helpful assistant. Please response to the user queries"),
-        ("user", "Question:{question}")
+        ("system", "You are a helpful assistant. Please respond to the user queries."),
+        ("user", "Question: {question}")
     ]
 )
 
 
-st.title('Langchain Demo With GEMINI API')
-input_text = st.text_input("Search the topic u want")
-
-
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash", 
+    model="gemini-2.5-flash",
     temperature=0.7,
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
 
-output_parser = StrOutputParser()
-chain = prompt | llm | output_parser
+OutPutParser = StrOutputParser()
 
-if input_text:
-    st.write(chain.invoke({'question': input_text}))
-    print(chain.invoke({'question': input_text}))
+chain = prompt | llm | OutPutParser
+
+
+
+#-----------------------------------
+# API Route
+#-----------------------------------
+@app.post("/ask")
+async def ask_llm(payload: QuesryRequest):
+
+    responce = chain.invoke({"question":payload.query})
+    return{
+        "question": payload.query,
+        "answer": responce
+    }

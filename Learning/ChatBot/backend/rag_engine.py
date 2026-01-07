78aba13baf6ef2f2d877b25d2c8e4428292184cf
Md. Tanvir Uddin Alif <alif252669@gmail.com>
2026-01-07 11:56:36 +0600

    tool implemented

--- Learning/ChatBot/backend/rag_engine.py
+++ Learning/ChatBot/backend/rag_engine.py
@@ -1,10 +1,13 @@
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.utilities import ArxivAPIWrapper, WikipediaAPIWrapper
+from langchain_community.tools import ArxivQueryRun, WikipediaQueryRun
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_classic.chains import create_retrieval_chain
 from langchain_community.vectorstores import Chroma
 from langchain_core.documents import Document
-from langchain_classic.chains import create_retrieval_chain
-from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.tools.retriever import create_retriever_tool
 
 
 import os
@@ -13,8 +16,35 @@ load_dotenv()
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
+
+
+
+
+#---------------------------------------
+# Reading from Arxiv(tool)
+#---------------------------------------
+
+arxiv_wrapper = ArxivAPIWrapper(top_k_results=5, doc_content_chars_max=2000)
+arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
+
+
+
+
+#---------------------------------------
+# Reading from WikiPidia(tool)
+#---------------------------------------
+
+wiki_wrapper = WikipediaAPIWrapper(top_k_results=5, doc_content_chars_max=2000)
+wiki = WikipediaQueryRun(api_wrapper=wiki_wrapper)
+
+
+
+
+#---------------------------------------
+# Reading from PDF
+#---------------------------------------
 
 file_path = '/home/md-tanvir-uddin-alif/Personal_Project/Gen_AI/Learning/ChatBot/SARCASM DETECTION IN PERSIAN.pdf'
 loader = PyPDFLoader(file_path)
@@ -25,7 +55,7 @@ full_text = " ".join([text.page_content for text in texts])
 
 
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=500)
 document = text_splitter.split_text(full_text)
 
 
@@ -39,6 +69,9 @@ db = Chroma.from_documents(docs, embeddings)
 
 retriever = db.as_retriever()
 
+retriever_tool = create_retriever_tool(retriever, 'SARCASM_DETECTION_IN_PERSIAN', "Search the Persian sarcasm detection research paper PDF for technical details, models, datasets, and experiments.")
+
+
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
@@ -47,21 +80,51 @@ llm = ChatGoogleGenerativeAI(
 )
 
 
-prompt = ChatPromptTemplate.from_template("""
-Answer the following question based only on the provided context. 
-Think step by step before providing a detailed answer. 
-I will tip you $1000 if the user finds the answer helpful. 
-
-Context:
-{context}
-
-Question: {input}
-""")
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """
+    You are a research assistant. 
+    Think step by step. Use tools when needed.
+    """),
+    ("human", "{input}"),
+    MessagesPlaceholder(variable_name="agent_scratchpad"),
+])
 
 
-document_chain = create_stuff_documents_chain(llm, prompt)
-retrieval_chain = create_retrieval_chain(retriever, document_chain)
+# document_chain = create_stuff_documents_chain(llm, prompt)
+# retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+tools = [wiki, arxiv, retriever_tool]
+
+
+from langchain.agents import create_agent
+from langchain_classic.agents import create_tool_calling_agent, AgentExecutor
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    tools=tools,
+    prompt=prompt
+)
+
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True
+)
+
+# parser = StrOutputParser()
 
 def get_answer(query: str):
-    response = retrieval_chain.invoke({"input": query}) 
-    return response["answer"]
+    response = agent_executor.invoke({"input": query})
+    output = response["output"]
+
+    if isinstance(output, list):
+        final_text = []
+        for part in output:
+            if isinstance(part, dict) and "text" in part:
+                final_text.append(part["text"])
+            elif isinstance(part, str):
+                final_text.append(part)
+        return "".join(final_text)
+
+    return str(output)
